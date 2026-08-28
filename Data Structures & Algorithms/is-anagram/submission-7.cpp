e83683d5117ee088fe01e35e4a6e65ce684e0d85
Sayaka Alam <156659954+SayakaMeem@users.noreply.github.com>
2026-08-28 23:48:55 +0600

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) { 
+        
+        if(s.length()!=t.length())
+        return false;
+        else{
+
+        int count[26]={0};
+
+        for(int i=0;i<s.length();i++) {
+        
+            count[s[i]-'a']++;
+            count[t[i]-'a']--;
+            
+            }
+        
+        for (int c:count){
+         if (c!=0) 
+         
+         return false;
+         
+        }
+         return true;
+        }
+    
+          
+    }
+};
